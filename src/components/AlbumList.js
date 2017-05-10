7e4f41ba978a75d548b23bea4c2c2a587fa71566
robabby <robabby23@gmail.com>
2017-05-09 20:18:20 -0500

    Converted AlbumList to a class

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
-  return (
-      <View>
-        <Text>Album List</Text>
-      </View>
-  );
-};
+  render() {
+    return (
+        <View>
+          <Text>Album List</Text>
+        </View>
+    );
+  }
+}
 
 export default AlbumList;
