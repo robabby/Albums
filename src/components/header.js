f4ae6d8b6d2995871225e36f33975030f1a46de0
robabby <robabby23@gmail.com>
2017-05-09 19:26:46 -0500

    Passing props to Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
 
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#f8f8f8'
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+
+    },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
   },
   textStyle: {
     fontSize: 20
