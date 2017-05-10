78792d4d7f8532b5d5af35a9af459fa3e10ce744
robabby <robabby23@gmail.com>
2017-05-09 20:15:47 -0500

    Implemented AlbumList

---
+++ src/components/AlbumList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+  return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+  );
+};
+
+export default AlbumList;
