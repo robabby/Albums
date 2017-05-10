e51b770dd315673f26e871ef924809a4feab282e
robabby <robabby23@gmail.com>
2017-05-09 20:30:37 -0500

    Added lifecycle method

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  componentWillMount() {
+    console.log('AlbumList has mounted');
+  }
   render() {
     return (
         <View>
